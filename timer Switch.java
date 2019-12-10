b638b85eafce03e16d545095d9fb44736cbcaae8
kostakar <56451882+kostakar@users.noreply.github.com>
2019-12-10 13:47:21 +0200

    Create timer Switch.java

---
+++ timer Switch.java
@@ -0,0 +1,15 @@
+public void autosave(){
+        Timer timer = new Timer();
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+
+
+
+            }
+        };
+
+        timer.schedule(timerTask,0,28800);
+
+    };
